0daf28b27d24ac227f4fb8f404d58370ac67e072
ammar-rasheed@hotmail.com <ammar-rasheed@hotmail.com>
2023-09-10 00:59:46 +0300

    Add deteils g14......

--- index.js
+++ index.js
@@ -44,34 +44,34 @@ const api2 = new threeCommasAPI({
 
 
 const capitalMap = new Map([
+  [31814867, { title: 'G13 2X 15M 0.58%', capital: 1000 }],
+  [32103676, { title: 'G14 2X 15M 0.58', capital: 2000 }],
+  [32474224,{title: 'G126 2X 15M 0.58%',capital: 1000}], 
+  
+  [32470971,{title: 'G128 2X 1H 1%',capital: 1000}], 
+  [32427154, { title: 'G124 2X 1H 1%', capital: 1000 }],
+
+  [32427159,{title: 'G125 2X 4H 1%',capital: 1000}], 
   [32101201, { title: 'G9 2X 4H 1%', capital: 1000 }],
-  [32470971,{title: 'G128 2X 2X 1H 1%',capital: 1000}], 
-  [31814867, { title: 'G13 15M 0.58%', capital: 1000 }],
-  [ 32101635,{title: 'G16 4X 15M 1% ',capital: 1000}], 
 
   [31876293, { title: 'G11 4X 15M 0.58%', capital: 1000 }],
-  [32103676, { title: 'G14', capital: 2000 }],
+  [32244371, { title: 'G118 4x 15M 0.58%', capital: 1000 }],
+  [ 32101635,{title: 'G16 4X 15M 1% ',capital: 1000}], 
+  [32476763,{title: 'G129 4X 15M 1%',capital: 1000}], 
+
+  [32244363, { title: 'G117 4X 1H 1%', capital: 1000 }],
+  [32260429, { title: 'G30 4X 1H 1%', capital: 1000 }],
+  [32152427, { title: 'G22 4X 1H 0.58%', capital: 1000 }],
+
+  
+
+  
   [32178454, { title: 'G24', capital: 1000 }],
   [32427107, { title: 'G122', capital: 1700 }],
-
-
-  [32427154, { title: 'G124 2X 1H 1%', capital: 1000 }],
-  [32427159,{title: 'G125 2X 4H 1%',capital: 1000}], 
-  [32474224,{title: 'G126 2X 15M 0.58%',capital: 1000}], 
-  [32476763,{title: 'G129 4X 15M 1%',capital: 1000}], 
-  [32244371, { title: 'G118 4x 15m 0.58%', capital: 1000 }],
-  [32244363, { title: 'G117 4X 1H 1%', capital: 1000 }],
-
-
-
   [32428979, { title: 'G66+1000', capital: 3000 }],
   [32433201, { title: 'G67', capital: 345 }],
-  //[32208556, { title: 'G27', capital: 1000 }],
   [32423630, { title: 'G98', capital: 427 }],
   [32435532, { title: 'G72', capital: 2000 }],
-  //[32152427, { title: 'G22', capital: 500 }],
-  [32260429, { title: 'G30', capital: 1000 }],
-  //[ 32244961,{title: 'G56 ',capital: 500}],
  
 ]);
 
@@ -99,7 +99,7 @@ function sendAlert(accountId, title, percentage) {
 app.get('/data', async (req, res) => {
   try {
     const api1Ids = [
-      32101201, 31876293, 32103676, 32178454, 32427154,
+      32101201, 31876293, 32103676, 32178454, 32427154,32152427,
       32427107, 32428979, 32433201, 31814867,32101635,32470971,32427159,32474224,
     ];
     const api2Ids = [
